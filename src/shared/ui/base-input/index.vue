b352a6925f2ebc7cce688eee2176e23de8f8a3d7
ValeraVasilevsky <valera200208@gmail.com>
2024-07-18 15:51:13 +0700

    feat: added type & mode attrs for base-input component

--- src/shared/ui/base-input/index.vue
+++ src/shared/ui/base-input/index.vue
@@ -8,6 +8,8 @@
       v-model="inputValue"
       :placeholder="props.placeholder"
       :class="formStyles.input"
+      :type="inputType.type"
+      :inputmode="inputType.inputMode"
       @input="onInput"
     />
 
@@ -27,16 +29,21 @@ interface BaseInputProps {
   placeholder?: string;
   error?: boolean;
   errorMessage?: string;
+  variant: "phone" | "password" | "text" | "email" | string;
+  required?: boolean;
 }
 type BaseInputEmits = {
   "update:value": [string];
 };
+type InputModeType = "text" | "tel" | "email";
 
 const props = withDefaults(defineProps<BaseInputProps>(), {
   value: "",
   placeholder: "",
   error: false,
   errorMessage: "",
+  variant: "",
+  required: false,
 });
 const emits = defineEmits<BaseInputEmits>();
 const slots = defineSlots<{
@@ -63,6 +70,26 @@ const formStyles = computed<{ input: string[]; container: string[] }>(
     return classes;
   }
 );
+const inputType = computed((): { inputMode: InputModeType; type: string } => {
+  if (props.variant === "phone") {
+    return {
+      inputMode: "tel",
+      type: "text",
+    };
+  }
+
+  if (props.variant === "email") {
+    return {
+      inputMode: "email",
+      type: "text",
+    };
+  }
+
+  return {
+    inputMode: "text",
+    type: "text",
+  };
+});
 
 const onInput = (): void => {
   emits("update:value", inputValue.value);
